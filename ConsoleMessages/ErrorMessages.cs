13f3ddd4f3dccd98e004115254963b6edaf2f002
rb <rkrb@tyfonmail.se>
2024-05-22 16:29:52 +0200

    Fixed typo in ErrorMessages.cs

--- ConsoleMessages/ErrorMessages.cs
+++ ConsoleMessages/ErrorMessages.cs
@@ -4,6 +4,6 @@ public static class ErrorMessages
 {
     public static void ErrorMessageInvalidUsrInput()
     {
-        Console.WriteLine("Enter a number between 1-3.");
+        Console.WriteLine("Enter a number between 0-3.");
     }
 }
