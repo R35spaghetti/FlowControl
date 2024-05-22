9b2782badcb52ed60eb8ea90e497c5032e17b523
rb <rkrb@tyfonmail.se>
2024-05-22 15:12:35 +0200

    Improved readability in the PresentOptions message in InformationMessages.cs

--- ConsoleMessages/InformationMessages.cs
+++ ConsoleMessages/InformationMessages.cs
@@ -9,7 +9,11 @@ public static class InformationMessages
 
     public static void PresentOptions()
     {
-        Console.WriteLine("\n**Choose between 0-3**");
+        Console.WriteLine("\n**Choose between 0-3**" +
+                          "\n 0: Exit program" +
+                          "\n 1: Order movie tickets" +
+                          "\n 2: Print your input 10 times in the console" +
+                          "\n 3: Print the third word in a sentence");
     }
 
     public static void ExitMessage()
